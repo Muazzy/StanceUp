e066615fc0210cd8697ab5b16d97baee4c5b1175
muazzam <muazzamafaque@gmail.com>
2023-10-09 22:13:45 +0500

    video screen i.e youtube feature done

--- README.md
+++ README.md
@@ -2,16 +2,19 @@
 - Profile screen - with details of body weight, height, age, and then it also shows the calculated bmr
 
 
-- show youtube videos screen
+<!-- /done -->
+<!-- - show youtube videos screen
     create a model
     implement youtube explode
     screen which shows listtile of the videos
         screen takes a keyword as a param and show videos according to that
-        when video is clicked it should open the video in the yt app
+        when video is clicked it should open the video in the yt app -->
 
-
+Beautify the ui of this video screen
 
         
     
 - add some other exercise/pose, like pushups
+
+
 - make sure that the analysis is working properly
--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -1,36 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:fyp/models/video.dart';
 import 'package:fyp/provider/video_provider.dart';
+import 'package:fyp/utils/app_colors.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class VideoScreen extends StatefulWidget {
-  const VideoScreen({super.key, required this.query, required this.title});
-
+class VideoScreen extends StatelessWidget {
   final String title;
   final String query;
 
-  @override
-  State<VideoScreen> createState() => _VideoScreenState();
-}
-
-class _VideoScreenState extends State<VideoScreen> {
-  @override
-  void initState() {
-    // context.read<VideoProvider>().search(widget.query);
-    super.initState();
-  }
-
+  const VideoScreen({super.key, required this.title, required this.query});
   @override
   Widget build(BuildContext context) {
     final videos = context.watch<VideoProvider>().videos;
     final bool isLoading = context.watch<VideoProvider>().isLoading;
-
-    print(videos);
-    print(isLoading);
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: GetBody(videos: videos, isLoading: isLoading),
     );
@@ -55,13 +41,91 @@ class GetBody extends StatelessWidget {
       );
     }
     return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(videos[index].title ?? ''),
-          subtitle: Text(videos[index].author ?? ''),
+        final video = videos[index];
+        return VideoTile(
+          video: video,
+          onTap: () {
+            openVideo(video.url ?? '');
+          },
         );
       },
       itemCount: videos.length,
     );
+  }
+}
+
+class VideoTile extends StatelessWidget {
+  const VideoTile({
+    super.key,
+    required this.video,
+    required this.onTap,
+  });
+
+  final VideoModel video;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        onTap: onTap,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        tileColor: AppColors.primaryColor.withOpacity(0.1),
+        dense: false,
+        isThreeLine: false,
+        minVerticalPadding: 16,
+        title: Text(
+          video.title ?? '',
+          style: const TextStyle(
+            color: AppColors.primaryColor,
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(video.author ?? ''),
+        leading: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          width: MediaQuery.of(context).size.width * 0.25,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              video.thumbnail ?? defaultThumbnail,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(video.duration ?? ''),
+            const SizedBox(width: 4),
+            const Icon(
+              Icons.watch_later,
+              size: 20,
+              color: AppColors.secondaryColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void openVideo(String url) async {
+  try {
+    await launchUrl(Uri.parse(url));
+    print("success $url");
+  } catch (e) {
+    print(e);
   }
 }
